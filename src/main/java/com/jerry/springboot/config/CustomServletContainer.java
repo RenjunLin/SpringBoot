5eba375a5f060891213ef131ad9fe3d1ff26a765
lrenjundk <linrenjun@egeio.com>
2017-07-28 13:43:40 +0800

    自定义servlet容器

---
+++ src/main/java/com/jerry/springboot/config/CustomServletContainer.java
@@ -0,0 +1,23 @@
+package com.jerry.springboot.config;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 通过代码来配置servlet容器，可以用于取代配置文件
+ */
+@Component
+public class CustomServletContainer implements EmbeddedServletContainerCustomizer{
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(8888);
+        container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404.html"));
+        container.setSessionTimeout(10, TimeUnit.MINUTES);
+    }
+}
