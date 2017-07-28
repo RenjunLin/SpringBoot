bda207a7d1ff98bce01c6152e918edb2dadfd2ee
lrenjundk <linrenjun@egeio.com>
2017-07-28 15:23:30 +0800

    修改容器为jetty

---
+++ src/main/java/com/jerry/springboot/config/CustomeServletConfig.java
@@ -0,0 +1,28 @@
+package com.jerry.springboot.config;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class CustomeServletConfig{
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        JettyEmbeddedServletContainerFactory factory = new JettyEmbeddedServletContainerFactory();
+        factory.setPort(8888);
+        factory.setSessionTimeout(10, TimeUnit.MINUTES);
+        factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404.html"));
+        return factory;
+    }
+
+}
