45e84843b685e9b7d6c7b7117a184f14a4b88107
lrenjundk <linrenjun@egeio.com>
2017-07-28 13:56:06 +0800

    添加页面

---
+++ src/main/java/com/jerry/springboot/config/Config.java
@@ -0,0 +1,11 @@
+package com.jerry.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+
+
+}
